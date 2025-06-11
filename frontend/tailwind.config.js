ff145cfb188d33d8cedd11edf9a83763992d2d6f
bensiauu <benjaminsiau@gmail.com>
2025-06-11 15:39:58 +0800

    fixed styles not being applied

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,10 @@
 export default {
-       content: [
-              "./index.html", // so Tailwind scans your base HTML
-              "./src/**/*.{js,jsx,ts,tsx}", // so Tailwind scans all React files under src/
-       ],
-       theme: {
-              extend: {},
-       },
-       plugins: [],
-};
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
